c418359eac10f2f00ec351e76ec32bc958641555
Fredrik <ique@3h.se>
2017-05-26 15:20:46 +0200

    Working version of vidup

---
+++ src/main.rs
@@ -0,0 +1,95 @@
+#![feature(plugin)]
+#![plugin(rocket_codegen)]
+
+extern crate rocket_contrib;
+extern crate rocket;
+extern crate rand;
+extern crate serde_json;
+
+#[cfg(test)] mod tests;
+
+use std::io;
+use std::path::{Path, PathBuf};
+use std::process::Command;
+use std::collections::HashMap;
+use std::fs::File;
+
+use rocket::Request;
+use rocket::response::Stream;
+use rocket_contrib::Template;
+
+use rocket::Data;
+
+use rocket::response::{NamedFile, Failure};
+use rocket::http::Status;
+use rand::{Rng};
+
+#[get("/assets/<file..>")]
+fn static_files(file: PathBuf) -> Option<NamedFile> {
+    NamedFile::open(Path::new("static/").join(file)).ok()
+}
+
+#[get("/")]
+fn index() -> Template {
+    let context: HashMap<String,String> = HashMap::new();
+    Template::render("index", &context)
+}
+
+#[post("/upload", data = "<data>")]
+fn upload(data: Data) -> Result<String, Failure> {
+    let s = rand::thread_rng().gen_ascii_chars().take(10).collect::<String>();
+    data.stream_to_file(format!("tmp/{}", s)).unwrap();
+    let output = Command::new("ffmpeg")
+                         .arg("-i")
+                         .arg(format!("tmp/{}", s))
+                         .arg("-vcodec")
+                         .arg("libx264")
+                         .arg("-vprofile")
+                         .arg("high")
+                         .arg("-vf")
+                         .arg("scale=-1:720")
+                         .arg("-threads")
+                         .arg("0")
+                         .arg("-acodec")
+                         .arg("libvo_aacenc")
+                         .arg("-b:a")
+                         .arg("128k")
+                         .arg(format!("files/{}.mp4", s))
+                         .output();
+    match output {
+        Ok(out) =>  {
+            if out.status.success() {
+                Ok(s)
+            } else {
+                Err(Failure(Status::NotAcceptable))
+            }
+        },
+        Err(_) => Err(Failure(Status::InternalServerError))
+    }
+}
+
+#[get("/<id>")]
+fn play(id: String) -> Template {
+    let mut context = HashMap::new();
+    context.insert("video", id);
+    Template::render("video", &context)
+}
+
+#[get("/files/<filename>")]
+fn video(filename: String) -> io::Result<Stream<File>> {
+    File::open(format!("files/{}", filename)).map(|file| Stream::from(file))
+}
+
+#[error(404)]
+fn not_found(req: &Request) -> Template {
+    let mut map = std::collections::HashMap::new();
+    map.insert("path", req.uri().as_str());
+    Template::render("error/404", &map)
+}
+
+fn main() {
+    rocket::ignite()
+        .mount("/", routes![index, upload, play, video, static_files])
+        .catch(errors![not_found])
+        .launch();
+}
---
+++ static/app.js
@@ -0,0 +1,59 @@
+$(function() {
+  $form = $('#upload-area');
+  $form.on('drag dragstart dragend dragover dragenter dragleave drop', function(e) {
+      e.preventDefault();
+      e.stopPropagation();
+    })
+    .on('dragover dragenter', function() {
+      $form.addClass('is-dragover');
+    })
+    .on('dragleave dragend drop', function() {
+      $form.removeClass('is-dragover');
+    })
+    .on('drop', function(e) {
+      droppedFiles = e.originalEvent.dataTransfer.files;
+      if (droppedFiles.length > 1)
+        $('.msg').html('You can only upload one file at a time');
+      upload(droppedFiles[0]);
+    });
+
+});
+
+function upload(file){
+  //var reader = new FileReader();
+  $('#upload-msg').hide();
+  $('#uploading-msg').show();
+  //reader.onload = function(e) {
+    $.ajax({
+      xhr: function() {
+        var xhr = new window.XMLHttpRequest();
+        xhr.upload.addEventListener("progress", function(evt) {
+          if (evt.lengthComputable) {
+            var percentComplete = evt.loaded / evt.total;
+            console.log(percentComplete);
+            $('#uploading-msg percent').html(Math.round(percentComplete * 100) + "%");
+          }
+       }, false);
+       xhr.upload.addEventListener("load", function(evt) {
+         $('#uploading-msg').hide();
+         $('#processing-msg').show();
+       }, false);
+
+       return xhr;
+      },
+      type: 'POST',
+      url: '/upload',
+      //data: event.target.result,
+      data: file,
+      processData: false,
+      contentType: false
+    }).done(function(data) {
+      window.location = "/"+data;
+    }).fail(function(data) {
+      $('#upload-msg').show();
+      $('#processing-msg').hide();
+      $('.upload-msg').html('Could not process file, unsupported file format.');
+    });
+  //};
+  //reader.readAsArrayBuffer(file);
+}
